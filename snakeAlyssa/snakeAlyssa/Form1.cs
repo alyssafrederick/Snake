47ccb6491f14b224ce71510deadb9d3dada36c57
alyssafrederick <peaceloveyumnmydance@gmail.com>
2017-07-08 11:17:38 -0700

    adding files

---
+++ snakeAlyssa/snakeAlyssa/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace snakeAlyssa
+{
+    public partial class snakeeee : Form
+    {
+        Graphics gfx;
+        List<Snake> snakes;
+
+        public snakeeee()
+        {
+            InitializeComponent();
+        }
+
+        private void snakeeee_Load(object sender, EventArgs e)
+        {
+            gfx = CreateGraphics();
+            timer.Start();
+            snakes = new List<Snake>();
+            snakes.Add(new Snake(30, 30, 5, 5));
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < snakes.Count; i++)
+            {
+                snakes[i].Update();
+                snakes[i].Render(gfx);
+            }
+        }
+
+        private void snakeeee_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left)
+            {
+                snakes[0].Direction = 1;
+            }
+
+            if (e.KeyData == Keys.Right)
+            {
+                snakes[0].Direction = 2;
+            }
+
+            if (e.KeyData == Keys.Up)
+            {
+                snakes[0].Direction = 3;
+            }
+
+            if (e.KeyData == Keys.Down)
+            {
+                snakes[0].Direction = 4;
+            }
+        }
+    }
+}
---
+++ snakeAlyssa/snakeAlyssa/Snake.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snakeAlyssa
+{
+    public class Snake
+    {
+        int _x;
+        int _y;
+        int _width;
+        int _height;
+        public int Direction;
+        //define a number as a direction
+        /* left = 1 -> x-5
+         * right = 2 -> x+5
+         * up = 3 -> y-5
+         * down = 4 -> y+5
+         */
+
+        public Snake(int x, int y, int width, int height)
+        {
+            _x = x;
+            _y = y;
+            _width = width;
+            _height = height;
+        }
+
+        public void Update()
+        {
+            if (Direction == 1)
+            {
+                _x -= 5;
+            }
+
+            if (Direction == 2)
+            {
+                _x += 5;
+            }
+
+            if (Direction == 3)
+            {
+                _y -= 5;
+            }
+
+            if (Direction == 4)
+            {
+                _y += 5;
+            }
+        }
+
+        public void Render(Graphics gfx)
+        {
+            gfx.FillRectangle(new SolidBrush(Color.White), _x, _y, _width, _height);
+        }
+    }
+}
